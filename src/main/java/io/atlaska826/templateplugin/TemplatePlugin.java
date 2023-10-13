02009274efe093926690da3100cf634426b0595d
Atlas Mallams <131003995+atlaska826@users.noreply.github.com>
2023-10-13 08:31:56 -0500

    Create TemplatePlugin.java

---
+++ src/main/java/io/atlaska826/templateplugin/TemplatePlugin.java
@@ -0,0 +1,13 @@
+package io.atlaska826.templateplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TemplatePlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+  
+    @Override
+    public void onDisable() {
+    }
+}
